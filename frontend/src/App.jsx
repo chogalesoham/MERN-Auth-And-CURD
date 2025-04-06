07f9682f5d8de1642095fcd9a4d27c539d64ed9b
soham_chogale <chogalesoham74@gmail.com>
2025-04-06 12:09:14 +0530

    setup/add frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" flex items-center justify-center">
+      <h1 className=" text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
