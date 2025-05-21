6c55527222e427c5248ce63e5150e21098811101
soham_chogale <chogalesoham74@gmail.com>
2025-05-21 17:13:17 +0530

    feat/add getall delete and update product api

--- backend/Controllers/product-controllers.js
+++ backend/Controllers/product-controllers.js
@@ -7,7 +7,11 @@ const createNewProduct = async (req, res) => {
     body.image = req.file ? req.file?.path : null;
     const product = new productModel(body);
     await product.save();
-    res.status(201).json({ message: "product created successfully", product });
+    res.status(201).json({
+      success: true,
+      message: "product created successfully",
+      data: product,
+    });
   } catch (error) {
     res.status(500).json({
       message: "Internal server error",
@@ -15,4 +19,80 @@ const createNewProduct = async (req, res) => {
   }
 };
 
-module.exports = { createNewProduct };
+// PUT - Update Product By Id
+const updateProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price } = req.body;
+    let updatedProductData = { name, description, price };
+    if (req.file) {
+      updatedProductData.image = req.file.path;
+    }
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      id,
+      updatedProductData,
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      message: "Product Updated",
+      data: updatedProduct,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+//GET - all products
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res
+      .status(200)
+      .json({ success: true, message: "all products", data: products });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//GET - Get product by id
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productModel.findById(id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "Product Not Found" });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "get product by Id", data: product });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+//DELETE - delete product by id
+const deleteProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productModel.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).json({ success: false, message: "Product Not Found" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Product Deleted Successful",
+      data: product,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  createNewProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+};
--- backend/Routes/products-routes.js
+++ backend/Routes/products-routes.js
@@ -1,4 +1,10 @@
-const { createNewProduct } = require("../Controllers/product-controllers");
+const {
+  createNewProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+} = require("../Controllers/product-controllers");
 const Authenticate = require("../Middlewares/auth");
 const { cloudinaryFileUploader } = require("../Middlewares/file-uploader");
 
@@ -9,14 +15,13 @@ router.post(
   cloudinaryFileUploader.single("image"),
   createNewProduct
 );
-
-router.get("/products", Authenticate, (req, res) => {
-  res.status(200).json([
-    { name: "product1", price: 100 },
-    { name: "product2", price: 200 },
-    { name: "product3", price: 300 },
-    { name: "product4", price: 400 },
-  ]);
-});
+router.put(
+  "/update-product/:id",
+  cloudinaryFileUploader.single("image"),
+  updateProductById
+);
+router.get("/products", getAllProducts);
+router.get("/products/:id", getProductById);
+router.delete("/products/:id", deleteProductById);
 
 module.exports = router;
